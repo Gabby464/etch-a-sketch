361fee428d8549e543d674576cae400c1d5d2091
Gabriela P <gabriela.poryazova@gmail.com>
2022-08-02 20:08:06 +0300

    Change logic behind creatin square elements

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ function createDivsForDrawing() {
     //create
     const newDiv = document.createElement("div");
     const divArray = [];
-    for(i = 1; i <= 256; i++){
+    for(i = 1; i <= 16; i++){
         const newDiv = document.createElement("div");
+        for(j = 1; j <= 16; j++){
+            const squareDiv = document.createElement("div");
+            newDiv.appendChild(squareDiv);
+            squareDiv.setAttribute("class", "square");
+        }
         drawingPadDiv.appendChild(newDiv);
         //add attributes
-        newDiv.setAttribute("class" , "square");
+        newDiv.setAttribute("class" , "grid-basis");
     }
+    
    
 
 }
