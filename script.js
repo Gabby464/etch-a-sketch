e5fe0c5d5732ffe255b4b0d31fd1d72d7aedf9a5
Gabriela P <gabriela.poryazova@gmail.com>
2022-08-04 23:33:53 +0300

    Create drawing pad by dimensions given by the client

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
 const drawingPadDiv = document.getElementById("drawing-pad");
-
+//16 cells by default but the user can change that at any given point
+let numberOfCells = 16; //default value
 //create and add attirbutes to divs for drawing
-function createDivsForDrawing() {
+function createDivsForDrawing(numberOfCells) {
     //create
     const newDiv = document.createElement("div");
+    drawingPadDiv.innerHTML = ""
     const divArray = [];
-    for(i = 1; i <= 16; i++){
+    let sizeOfDivs = 40 / Number(numberOfCells)
+    for (i = 1; i <= numberOfCells; i++) {
         const newDiv = document.createElement("div");
-        for(j = 1; j <= 16; j++){
+        for (j = 1; j <= numberOfCells; j++) {
             const squareDiv = document.createElement("div");
             newDiv.appendChild(squareDiv);
+            squareDiv.style.height = sizeOfDivs + "em";
+            squareDiv.style.width = sizeOfDivs + "em";
             squareDiv.setAttribute("class", "square");
         }
         drawingPadDiv.appendChild(newDiv);
         //add attributes
-        newDiv.setAttribute("class" , "grid-basis");
+        newDiv.setAttribute("class", "grid-basis");
     }
+
 }
-createDivsForDrawing()
-
-//declare all square elements globally
-const squareDivs = document.getElementsByClassName("square");
-const squareDivsArray = Array.from(squareDivs);
-
-function addHoverFunctionality(){
+createDivsForDrawing(16) //declaring the function with the default value, so when the HTML loads a 16x16 grid loads 
+//add functionality to all created divs, so they can change colors on hover;
+function addHoverFunctionality() {
+    const squareDivs = document.getElementsByClassName("square");
+    const squareDivsArray = Array.from(squareDivs);
     squareDivsArray.forEach((element) => {
         element.addEventListener('mouseover', ((e) => {
             e.target.style.backgroundColor = "#FFC0CB"
         }))
     })
 }
+//next create Pop-up and edit grid measures - https://www.w3schools.com/howto/howto_js_popup_form.asp
 addHoverFunctionality()
+//pop-up created -> add show/hide functionality onclick;
+const popUpEl = document.getElementsByClassName("pop-up")
+function popUpFuncrionality() {
+    const buttonEl = document.getElementsByClassName("option");
+    buttonEl[0].addEventListener("click", () => {
+        popUpEl[0].style = "display: block";
+        submitNewGrid()
+    })
+}
+popUpFuncrionality()
+// submit new grid option and turn it into the new drawing pad
+function submitNewGrid() {
+    const inputEl = document.getElementById("cells-number");
+    const label = document.getElementById("size-label")
+    inputEl.addEventListener("mousemove", () => {
+        label.textContent = inputEl.value
+    })
+    const confirmEl = document.getElementById("confirm")
+    confirmEl.addEventListener("click", () => {
+        numberOfCells = inputEl.value
+        createDivsForDrawing(numberOfCells)
+        addHoverFunctionality()
+        //creating + adding functionality
+        popUpEl[0].style = "display: none";
+    })
+}
